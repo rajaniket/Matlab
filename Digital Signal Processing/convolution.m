f9b831d461f751f607001d362eab7e256295a0eb
Aniket Raj <aniketraj.go@gmail.com>
2019-11-09 20:32:47 +0530

    linear convolution of signal

--- Digital Signal Processing/convolution.m
+++ Digital Signal Processing/convolution.m
@@ -1,7 +1,9 @@
+%Prog to find the linear convolution of signal
+
 clc;
 clear;
 close all;
-xn=input('type the range of x[n] :');
+xn=input('type the range of x[n] :'); 
 hn=input('type the range of h[n] :');
 x=input('type the value of x[n] :');
 h=input('type the value of h[n] :');
@@ -10,9 +12,9 @@ m=length(h);
 yn=min(xn)+min(hn):max(xn)+max(hn); %range of convln
 k=length(yn);
 y=zeros(1,k);
-for i=1:k
-    for j=1:l
-        p=i-j+1;
+for i=1:k   % i can be treated as n of convolution formula
+    for j=1:l  % k can be treated as k of convolution formula
+        p=i-j+1; % p is something like (n-k) & +1 is for its matrix valadiation
         if(p>0 && p<m+1)
             y(1,i)=y(1,i)+x(1,j)*h(1,p);
         end
