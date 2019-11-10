56b44e9628e1713f0008a75d08a14449b33a9f30
Aniket Raj <aniketraj.go@gmail.com>
2019-11-10 21:46:23 +0530

    Impulse response of transfer function

---
+++ Digital Signal Processing/E4_impls_resp_of_TF.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+close all;
+n=input('enter the numerator:');
+d=input('enter the denomerator:');
+N=input('input the sampple: ');
+subplot(221)
+zplane(n,d);
+title('pole zero plot');
+[m,t]=impz(n,d,N);
+subplot(222);
+stem(t,m);
+xlabel('Time');
+ylabel('Magnitude')
+title('F[n]');
+disp(m);
