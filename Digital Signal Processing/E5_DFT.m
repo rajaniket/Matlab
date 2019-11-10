0b6aa3b09b7bd3b8800a192fc90f0f190a7f06f3
Aniket Raj <aniketraj.go@gmail.com>
2019-11-10 21:46:49 +0530

    Add files via upload

---
+++ Digital Signal Processing/E5_DFT.m
@@ -0,0 +1,27 @@
+clc;
+clear;
+close all;
+xn=input('Enter the value of x[n]:');
+n=input('Enter the range of x[n]:');
+l=length(xn);
+xk=[zeros(1,l)];
+w=exp(-2*i*pi/l);
+for i=1:l
+   for j=1:l
+       xk(1,i)=xk(1,i)+xn(1,j)* w^((i-1)*(j-1));
+   end
+end
+p=abs(xk);
+disp(xk);
+subplot(221)
+stem(n,xn);
+xlabel('magnitude');
+ylabel('time');
+title('x[n]');
+grid on;
+subplot(222)
+stem(n,p);
+xlabel('magnitude');
+ylabel('time');
+title('X[k]');
+grid on;
