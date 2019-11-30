25fb102649ec3e325d9d3e4ee9655234e3030f43
Aniket Raj <aniketraj.go@gmail.com>
2019-11-30 13:42:52 +0530

    Add files via upload

---
+++ control system/fbs_all.m
@@ -0,0 +1,19 @@
+%parallel system transfer function
+clc;
+clear;
+close all;
+n1=[1 0];
+d1=[0 1];
+n2=[0 5];
+d2=[0 1];
+n3=[0 1];
+d3=[1 1 0];
+n4=[0 1];
+d4=[0 1];
+[np,dp]=parallel(n1,d1,n2,d2);
+printsys(np,dp);
+[ns,ds]=series(np,dp,n3,d3);
+printsys(ns,ds);
+[nf,df]=feedback(ns,ds,n4,d4);
+printsys(nf,df);
+
