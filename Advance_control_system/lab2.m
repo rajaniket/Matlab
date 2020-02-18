eba500aae6679e2e1fe3c5bee8e87e1fa7f26512
Aniket Raj <aniketraj.go@gmail.com>
2020-02-18 23:37:58 +0530

    Update lab2.m

--- Advance_control_system/lab2.m
+++ Advance_control_system/lab2.m
@@ -1,8 +1,35 @@
-% Plot roots of given transfer function
-clc;
-clear;
-close all;
-n1=[8 56 96];
-d1=[1 4 9 10 0]
-[z,p,k]=tf2zp(n1,d1)
-pzmap(n1,d1);
+clc
+close all
+clear all
+n1=[0 0 1];
+d1=[1 2 16];
+gs=tf(n1,d1);
+for z=1:5
+    n2=[1 z];
+    d2=[0 1];
+    ds=tf(n2,d2);
+    Gs=gs*ds;
+    Gs_CL=feedback(Gs,1);
+    step(Gs_CL)
+    hold on
+    S=stepinfo(Gs_CL);
+    TR(z)=S.RiseTime;
+    ST(z)=S.SettlingTime;
+    OS(z)=S.Overshoot;
+    
+    %axis([0,5,0,0.4])
+    %figure
+end
+    z=1:5
+    k=-z
+    figure()
+    subplot(311)
+    plot(k,TR(z))
+    xlabel('-z')
+    ylabel('RT')
+    subplot(312)
+    plot(k,ST(z))
+    subplot(313)
+    plot(k,OS(z))
+    
+
