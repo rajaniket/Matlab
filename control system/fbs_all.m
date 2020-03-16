762079c3309ab10d41d0134809094131d2d64a45
Aniket Raj <aniketraj.go@gmail.com>
2020-03-16 23:29:55 +0530

    Solve the block and obtain transfer function

--- control system/fbs_all.m
+++ control system/fbs_all.m
@@ -10,10 +10,10 @@ n3=[0 1];
 d3=[1 1 0];
 n4=[0 1];
 d4=[0 1];
-[np,dp]=parallel(n1,d1,n2,d2);
+[np,dp]=parallel(n1,d1,n2,d2); % parallel blocks
 printsys(np,dp);
-[ns,ds]=series(np,dp,n3,d3);
+[ns,ds]=series(np,dp,n3,d3);  % series block
 printsys(ns,ds);
-[nf,df]=feedback(ns,ds,n4,d4);
+[nf,df]=feedback(ns,ds,n4,d4); % closed loop
 printsys(nf,df);
 
