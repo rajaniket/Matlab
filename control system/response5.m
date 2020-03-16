714b2a68ea815006c7b7a06cebbca3f17e2f2cec
Aniket Raj <aniketraj.go@gmail.com>
2020-03-16 23:27:41 +0530

    step response for transfer function

--- control system/response5.m
+++ control system/response5.m
@@ -4,7 +4,7 @@ clear;
 close all;
 n1=[0 36];
 d1=[1 3 36];
-sys=tf(n1,d1);
-printsys(n1,d1);
+sys=tf(n1,d1); % tranfer function
+printsys(n1,d1); 
 t=0:0.05:2;
-step(sys,t);
+step(sys,t);  % step response
