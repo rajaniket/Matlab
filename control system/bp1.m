6499262364e92a0fe2679703e697e19faee439a7
Aniket Raj <aniketraj.go@gmail.com>
2020-03-18 05:35:25 +0530

    bode plot

--- control system/bp1.m
+++ control system/bp1.m
@@ -4,9 +4,9 @@ clear;
 close all;
 n=[200 600];
 d=conv(conv([1,0],[1,2]),[1,4,100]);
-w=logspace(-1,3,100);
+w=logspace(-1,3,100); % logrithmic graph
 [mag,phase,w]=bode(n,d,w);
-magdb=20*log10(mag);
+magdb=20*log10(mag);  % magnitude 
 for i=1:10
     fprintf('%f \t\t\t %f\t\t\t%f\t\t\t\n',magdb(i),phase(i),w(i));
 end;
