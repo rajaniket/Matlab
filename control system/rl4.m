889883f2b08551e82ee0dfb5a4e3c41a37fe8774
Aniket Raj <aniketraj.go@gmail.com>
2020-04-29 23:51:25 +0530

    find root locous of given transfer function

--- control system/rl4.m
+++ control system/rl4.m
@@ -3,8 +3,8 @@ clc;
 clear;
 close all;
 n=[1];
-d=conv(conv([1,0],[1,4]),[1,5]);
+d=conv(conv([1,0],[1,4]),[1,5]); %conv(u,v) convolves vectors u and v . Algebraically, convolution is the same operation as multiplying the polynomials whose coefficients are the elements of u and v .
 sgrid(0.5,[]);
-[k,r]=rlocfind(n,d);
+[k,r]=rlocfind(n,d); % root locous
 disp(k);
 disp(r);
