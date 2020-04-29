b0fdf1af16c99c237b0375d5955af6389b4ec352
Aniket Raj <aniketraj.go@gmail.com>
2020-04-29 23:48:55 +0530

    exponential Response of transfer function

--- control system/response4.m
+++ control system/response4.m
@@ -4,8 +4,8 @@ clear;
 close all;
 n1=[1 5];
 d1=[1 2 3 5];
-sys=tf(n1,d1);
+sys=tf(n1,d1); % transfer function
 printsys(n1,d1);
-t=0:0.15:9;
-x=exp(-0.2*t);
+t=0:0.15:9; % t will vary 0 to 9 in step of 0.15
+x=exp(-0.2*t); 
 lsim(sys,x,t);
