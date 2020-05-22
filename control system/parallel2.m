99b2a5f1c880e97fc4b94507f058d845e21aa5a2
Aniket Raj <aniketraj.go@gmail.com>
2020-05-22 23:52:06 +0530

    Create parallel2.m

---
+++ control system/parallel2.m
@@ -0,0 +1,10 @@
+% Transfer function of Parallel blocks
+clc;
+clear;
+close all;
+n1=[0 5 1];
+d1=[0 1 5];
+n2=[0 2];
+d2=[0 1 2];
+[ns,ds]=parallel(n1,d1,n2,d2); %parallel blocks
+printsys(ns,ds);
